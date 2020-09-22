881cd3b2b2a64ddd5fb553db47e4c923c4e60884
ylc <2936453117@qq.com>
2020-09-22 08:59:10 +0800

    添加项目文件。

---
+++ ConsoleApp/DrawXml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public abstract class DrawXml : IDrawXml
+    {
+        void IDrawXml.Create(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDrawXml.CreateGrap(int hight, int weight, string name, IAspect Aspect, string strokewidth)
+        {
+        }
+
+        public enum Aspect
+        {
+            variable,
+            fix,
+        }
+
+
+    }
+
+}
---
+++ ConsoleApp/IDrawXml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+   public interface IDrawXml
+    {
+        #region 默认创建图形
+        void Create(string Name);
+        #endregion
+
+        #region 开始创建图形的宽高 name：图形名称   aspect：缩放时是否保持宽高比   strokewidth:线条宽度
+        void CreateGrap(int hight, int weight,string name, IAspect Aspect,string strokewidth);
+        #endregion
+
+        /// <summary>
+        /// 添加图形连接点坐标
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        void AddConnection(double x,double y);
+
+
+
+
+        /// <summary>
+        /// 结束创建图形
+        /// </summary>
+        void EndGrap();
+
+
+
+    }
+    public enum Aspect
+    {
+        variable,
+        fix,
+
+    }
+
+
+    public interface IAspect
+    {
+         string Fix { get; set; }
+
+    }
+}
